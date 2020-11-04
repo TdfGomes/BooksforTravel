a96ad9d6894d848c962f7e7344145cde6c07ba36
Tiago Gomes <t.dfgomes@icloud.com>
2020-11-04 13:42:53 +0000

    chore: comments

--- src/hooks/useWidth.js
+++ src/hooks/useWidth.js
@@ -2,19 +2,19 @@ import { useState, useEffect } from "react";
 
 /**
  *
- * @param {object} ref is a react reference for a DOM element
- * @param {string} destination
+ * @param {object} ref is a react reference for a DOM element.
+ * @param {string} destination is used to run this effect everytime we set a new destination and then a new set of books and we need it to set the current slider's width
  */
 
 function useWidth(ref, destination) {
   const [sliderWidth, setWidth] = useState(0);
 
-  const items = Array.from(ref?.getElementsByTagName("li") || []);
+  const items = Array.from(ref?.children || []);
 
   useEffect(() => {
     if (items.length) {
       const totalWith = items.reduce(
-        (acc, curr) => acc + curr.offsetWidth + 30, // sum the total width plus 30 from the element's horizontal padding
+        (acc, curr) => acc + curr.offsetWidth + 30, // sum the item's width plus 30px from the element's horizontal padding
         0
       );
       setWidth(totalWith);
